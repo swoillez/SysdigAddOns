2bb43d0d8cbd254fdc68204481fab888d1f013d6
Stephan Woillez <swoillez@hotmail.com>
2020-03-04 18:22:05 +0100

    correction of the build command

--- dockerPsResponseTime/startProbe.sh
+++ dockerPsResponseTime/startProbe.sh
@@ -2,7 +2,7 @@
 
 echo "-- Build the container image"
 
-docker build -f Dockerfile-Alpine -t stephw/dockerps-response-time:alpine .
+docker build -f Dockerfile-Alpine -t stephw/dockerps-response-time:alpine -t stephw/dockerps-response-time:latest .
 
 echo "-- Create the necessary environment ariables"
 
@@ -39,4 +39,4 @@ docker run -d --name $CONTAINER_NAME \
 -e DOCKER_HOST=${DOCKER_HOST} -e CLUSTER_NAME=${CLUSTER_NAME} \
 -e CHECK_INTERVAL=${CHECK_INTERVAL} -e METRIC_LABELS=${METRIC_LABELS} \
 -e MILLISECONDS=${MILLISECONDS} \
-stephw/dockerps-response-time:alpine
+stephw/dockerps-response-time
